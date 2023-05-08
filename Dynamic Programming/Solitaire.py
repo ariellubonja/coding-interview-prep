a3c33a98a59363d3516a8255ab014845e52a3701
Ariel Lubonja <ariel@cs.jhu.edu>
2023-05-08 13:48:01 +0300

    DP first - not limiting to jump of 6

--- Dynamic Programming/Solitaire.py
+++ Dynamic Programming/Solitaire.py
@@ -12,47 +12,47 @@
 
 
 def solution(A):
+    a = solve_subsequence(A)
+    if A[0] < 0:
+        a += A[0] # Need to add starting point. It wasn't added if negative
+    return a
     # Start from A[-6:]
     # Find Max Sum solution
     # ???
     # Transition from last 7 to previous unclear.
     # I guess go back 7 from start of max sequence for last 7?
-
-    max_sequence = []
-
-    i = len(A)
-    max_sum = 0
-    while i != 0:
-        start = max(i-7, 0)
-        current_seq = A[start:i]
-
-        sol_seq, seq_sum = solve_7_seq(A, current_seq)
-        max_sequence.insert(0, sol_seq) # Insert list at start of current
-        i = sol_seq[0]
-        max_sum += seq_sum
-
-
-    return max_sum
+    #
+    # max_sequence = []
+    #
+    # i = len(A)
+    # max_sum = 0
+    # while i != 0:
+    #     start = max(i-7, 0)
+    #     current_seq = A[start:i]
+    #
+    #     sol_seq, seq_sum = solve_7_seq(A, current_seq)
+    #     max_sequence.insert(0, sol_seq) # Insert list at start of current
+    #     i = sol_seq[0]
+    #     max_sum += seq_sum
+    # return max_sum
 
 
-def solve_7_seq(A, seq):
-    # [1, -2, 0, 9, -1, -2]
-    # Sln: 0, 3, 2 - dice roll
 
-    sln_path = []
-    maxSum = 0
-    for i in range(len(seq) - 1):
-        # If number ahead is positive, add to solution
-        if A[i] > 0:
-            sln_path.append(i)
-            maxSum += A[i]
 
-    sln_path.append(len(seq) - 1)  # We need to get to the last element of the current sequence to "exit" the game
+# TODO Add limit of 6-sided die jumps
+def solve_subsequence(seq):
+    # whether_to_add_curr + min(subsequence)
+    if len(seq) == 1:
+        return seq[0]
+    else:
+        if seq[0] > 0:
+            return seq[0] + solve_subsequence(seq[1:])
+        else:
+            return solve_subsequence(seq[1:])
 
-    maxSum += A[len(seq) - 1]
-
-    return sln_path, maxSum
 
 
 if __name__ == '__main__':
     assert solution([1, -2, 0, 9, -1, -2]) == 8
+    assert solution([-4, -10, -7]) == -11
+    assert solution([0, -4, -5, -2, -7, -9, -3, -10]) == -12

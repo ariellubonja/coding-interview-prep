4d73934d5f39584165a245b20f56e86ccef5a184
Ariel Lubonja <ariel@cs.jhu.edu>
2023-12-10 21:49:20 -0500

    MaxHeapify is correct I think. Need to add BuildHeap

--- heap.py
+++ heap.py
@@ -2,13 +2,14 @@
 
 # TODO deal with tab issue when selecting newline here in sublime
 
-heap_size = 1 # 1st element is sorted by default
+heaped = 1 # 1st element is sorted by default
 
 # TODO what does this do?
 def heapify(A):
 	# TODO I need to keep track of what is sorted and what isn't
 
-	return maxHeapify(A, 0)
+	maxHeapify(A, 0)
+	return A
 
 
 def mergeHeaps():
@@ -18,7 +19,7 @@ def mergeHeaps():
 	# TODO What if we have 3 mini-heaps and we need to merge them?
 	# Merge first 2, then merge that w/ 3rd (methinks)
 
-
+	raise NotImplementedError
 
 	pass
 
@@ -34,7 +35,7 @@ def getChild(i, side='left'):
 	elif side == 'right':
 		ind = 2*i + 2
 	else:
-		throw IllegalStateException # TODO look this up
+		raise ValueError("Child can only be 'left' or 'right', not", side)
 
 	return ind
 
@@ -48,87 +49,49 @@ def getChild(i, side='left'):
 
 
 def insert():
-	pass
+	raise NotImplementedError
 
 
 def maxHeapify(A, root):
 	if len(A) <= 1:
 		return
-	# heaped = len(A) # At the start, A is unsorted
+	# heaped = len(A) # At the start, A is unsorted. How to keep track?
 	left = getChild(root, 'left')
 	right = getChild(root, 'right')
-	# root = A[i_root]
 
-	if left >= len(A): # If left doesn't exist
+	# If left doesn't exist
+	if left >= len(A):
 		if right < len(A):
 			# Tree should be balanced left-to-right
 			raise ValueError("Left child doesn't exist while right child does. This is impossible")
 
-		# Tree is heaped so far, return
-		# Ya I don't think that's right.
-		# TODO I don't know how to maintain HeapSize state
-		# heap_size = max(heap_size, index)
 		return
 
 	largest = root
+	old_root = A[root]
 
 	if A[left] > A[largest]:
-		if right < len(A) and A[right] > A[left]: # Check if right is not out-of-bounds
+		largest = left
+
+	if right < len(A) and A[right] > A[left]: # Check if right is not out-of-bounds
+		if largest == left and A[right] > A[left]:
 			largest = right
-		else:
-			largest = left
-		old_root = A[root]
 
-		A[root] = A[largest] # Swap w/ the largest element
+	# Swap w/ the largest element
+	A[root] = A[largest]  # No effect if root is largest
 
-		if largest == left:
-			A[left] = old_root
-			maxHeapify(A, left)
-		elif largest == right:
-			A[right] = old_root
-			maxHeapify(A, right)
+	if largest == left:
+		A[left] = old_root
+		maxHeapify(A, left)
+	elif largest == right:
+		A[right] = old_root
+		maxHeapify(A, right)
 
+	# heaped = max(left, right) # TODO does this work with None?
+	# if heaped < len(A):
 
-		  # equivalent to elif len(A) == 2:
-		# TODO What is a min_value for python?
-
+	# TODO What is a min_value for python?
 	return
-	# # if len(A) > 3: # This is not the minimum tree
-	# # 	a = maxHeapify(A, 3)
-	# # 	A[3:] = a
-	# # TODO deal with joining these
-	#
-	#
-	#
-	#
-	# right = A[2]
-	#
-	# largest = root
-	#
-	# if left > largest: # Left child is bigger
-	# 	largest = left
-	#
-	# if right > largest: # Right child is bigger
-	# 	largest = right
-	#
-	# # Swap elements
-	# if largest != root: # if root is definitely not the largest element
-	# 	if left >= right: # Default to left child if equal
-	# 		largest = left # Left is the largest element
-	# 		root = largest
-	# 		left = root # Move root down left
-	# 	else:
-	# 		largest = right
-	# 		right = root
-	# 		root = largest
-	#
-	# # TODO is this pointer-based?
-	# # I think primitives are passed by value in Python. Yes, they need changing
-	# A[0] = root
-	# A[1] = left
-	# A[2] = right
-	#
-	# return A
 
 
 
